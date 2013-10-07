b3ddd87b2bb46a234aa740b25cb505d131ce3809
vinit kumar <vinit.kumar@changer.nl>
2013-10-07 08:09:19 +0530

    add update codew

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,10 +1,3 @@
-chrome.extension.onRequest.addListener(function(e, t, n) {
-    "use strict";
-    window.u = e.url, window.c = e.cat, window.product = e.product;
-});
-
-chrome.tabs.onUpdated.addListener(Facebook.onFacebookLogin);
-
 var Facebook = {
   /**
    * [onFacebookLogin Gets the temporary access token from facebook]
@@ -213,3 +206,25 @@ var Facebook = {
     }
   }
 };
+
+var api = null;
+
+function getTwitterAPI() {
+  if (api === null) {
+    api = new Twitter();
+  }
+
+  return api;
+}
+
+chrome.extension.onRequest.addListener(function(req, sender, res) {
+  getTwitterAPI().sign(req.verifier, res);
+});
+
+chrome.extension.onRequest.addListener(function(e, t, n) {
+    "use strict";
+    window.u = e.url, window.c = e.cat, window.product = e.product;
+});
+
+chrome.tabs.onUpdated.addListener(Facebook.onFacebookLogin);
+
