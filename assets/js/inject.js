cfbce6a69ee358a47950d4ca3e8cdd35bc0ef807
vinit kumar <vinit.kumar@changer.nl>
2013-10-07 11:27:51 +0530

    remove not needed fils

--- assets/js/inject.js
+++
@@ -1,9 +0,0 @@
-var temp = $("[itemprop=title]")[1], anot = $('[class="basestore"]'), cat, prod = $('[itemprop=name]').text();
-
-if (temp) {
-    cat = temp.innerText;
-} else {
-    cat = anot.text();
-}
-chrome.extension.sendRequest({ url: document.URL, cat: cat, product: prod});
-
